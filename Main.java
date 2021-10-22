80badcc8ff84782f466dddc3866b8e2c6c0ec086
Shella <anandashella59@gmail.com>
2021-10-22 12:38:18 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Shella", 20, 164);
+      hero.profile();
+      
+  
+       // jalankan aktivitas disini (GROUP 7)...
+       hero.jalan();
+
+       hero.lari();
+       hero.lari();
+       hero.lari();
+ 
+       hero.makan();
+ 
+       hero.minum();
+ 
+       hero.lompat();
+       hero.lompat();
+ 
+       hero.duduk();
+       
+       // tampilkan status terkini...
+       hero.profile();
+       
+     }
+   }
